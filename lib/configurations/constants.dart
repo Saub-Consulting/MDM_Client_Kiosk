d0c2724d963f9540822659d862e015f3acf5c966
Shatanik Mahanty <shatanikmahanty@gmail.com>
2024-10-20 20:03:03 +0530

    Update design as per requirements

---
+++ lib/configurations/constants.dart
@@ -0,0 +1,6 @@
+const androidCallIntent = 'android.intent.action.CALL';
+const kPadding = 8.0;
+const supportPhoneNumbers = [
+  '+917014261395',
+  '+919314296052',
+];
--- lib/pages/locked_page.dart
+++ lib/pages/locked_page.dart
@@ -1,82 +1,84 @@
 import 'package:android_intent_plus/android_intent.dart';
 import 'package:flutter/material.dart';
+import 'package:mdm_client/configurations/constants.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:qr_flutter/qr_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class LockedPage extends StatelessWidget {
   const LockedPage({super.key});
-  void _launchDialer([String? phoneNumber]) async {
-    final Uri phoneLaunchUri = Uri(scheme: 'tel', path: phoneNumber);
 
-    if (await canLaunchUrl(phoneLaunchUri)) {
-      await launchUrl(phoneLaunchUri);
-    } else {
-      print('Could not launch $phoneLaunchUri');
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(kPadding * 2),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: kPadding * 3),
+              const Text(
+                'Your device was locked as you have not paid your EMI',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: kPadding * 2),
+              const Text(
+                'आपका मोबाइल लॉक कर दिया गया है ॰ लॉक खुलवाने के लिए कॉल करें',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: kPadding * 2),
+              const Text('Call Tejpal Jadav to unlock', textAlign: TextAlign.center, style: TextStyle(fontSize: 20)),
+              const SizedBox(height: kPadding * 2),
+              ListView.builder(
+                itemCount: supportPhoneNumbers.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return _PhoneCallWidget(
+                    phoneNumber: supportPhoneNumbers[index],
+                  );
+                },
+              ),
+              const SizedBox(height: kPadding * 2),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+}
 
-  initiateCall() async {
+class _PhoneCallWidget extends StatelessWidget {
+  final String phoneNumber;
+
+  const _PhoneCallWidget({required this.phoneNumber});
+
+  Future<void> initiateCall(String phoneNumber) async {
     AndroidIntent intent = AndroidIntent(
-      action: 'android.intent.action.CALL',
-      data: Uri(scheme: 'tel', path: '7014261395').toString(),
+      action: androidCallIntent,
+      data: Uri(scheme: 'tel', path: phoneNumber).toString(),
     );
     await intent.launch();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
+    return GestureDetector(
+      onTap: () async {
+        final permission = await Permission.phone.request();
+        if (permission.isGranted) {
+          initiateCall(phoneNumber);
+        }
+      },
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(height: 20),
-            const Text(
-              'Your device was locked as you have not paid your EMI',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            Container(height: 10),
-            const Text('Call Tejpal Jadav to unlock', textAlign: TextAlign.center, style: TextStyle(fontSize: 20)),
-            Container(height: 10),
-            GestureDetector(
-              onTap: () async {
-                final permission = await Permission.phone.request();
-                if (permission.isGranted) {
-                  initiateCall();
-                }
-              },
-              child: const Text(
-                '+91 9876543210',
-                style: TextStyle(fontSize: 20, color: Colors.blue),
-              ),
-            ),
-            Container(height: 10),
-            OutlinedButton(
-                onPressed: () {
-                  _launchDialer();
-                },
-                child: const Text("Emergency Call")),
-            Container(height: 10),
-            const Spacer(
-              flex: 1,
-            ),
-            const Text('scan and pay using qr'),
-            QrImageView(
-              data: 'Tejpal Jadav to unlock +91 9876543210',
-              version: QrVersions.auto,
-              size: 300.0,
-            ),
-            const Spacer(
-              flex: 3,
-            )
-          ],
+        padding: const EdgeInsets.all(kPadding),
+        child: Text(
+          phoneNumber,
+          style: const TextStyle(fontSize: 20, color: Colors.blue),
+          textAlign: TextAlign.center,
         ),
       ),
-    ));
+    );
   }
 }
